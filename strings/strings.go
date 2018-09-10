646fc3d19324601f5a4e6547c8b4c40e03e2c557
Joan Lopez <joanjan14@gmail.com>
2018-09-10 09:13:01 +0200

    Adding new strings function

--- strings/strings.go
+++ strings/strings.go
@@ -1,9 +1,22 @@
 package strings
 
-import "strconv"
+import (
+	"strconv"
+	"strings"
+)
 
 type Fn func(a interface{}) string
 
 func IntFn(a interface{}) string {
 	return strconv.Itoa(a.(int))
+}
+
+func JoinByIgnoring(s []string, by, ignore string) string {
+	var r []string
+	for _, str := range s {
+		if str != ignore {
+			r = append(r, str)
+		}
+	}
+	return strings.Join(r, by)
 }
